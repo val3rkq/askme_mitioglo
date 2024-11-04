8434640ebbbea4e41002898c87c275cea555de97
Valery <87361814+val3rkq@users.noreply.github.com>
2024-11-04 21:33:56 +0300

    url hardcode fixed

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Главная страница
-    path('hot/', views.hot_questions, name='hot_questions'),  # Список "лучших" вопросов
-    path('tag/<str:tag>/', views.questions_by_tag, name='questions_by_tag'),  # Вопросы по тэгу
-    path('question/<int:question_id>/', views.question_detail, name='question_detail'),  # Один вопрос
+    path('hot/', views.hot_questions, name='hot'),  # Список "лучших" вопросов
+    path('tag/<str:tag>/', views.questions_by_tag, name='tag'),  # Вопросы по тэгу
+    path('question/<int:question_id>/', views.question_detail, name='question'),  # Один вопрос
     path('login/', views.login_view, name='login'),  # Форма входа
     path('signup/', views.signup_view, name='signup'),  # Форма регистрации
     path('settings/', views.settings_view, name='settings'),  # Форма настроек
-    path('ask/', views.ask_question, name='ask_question'),  # Создание вопроса
+    path('ask/', views.ask_question, name='ask'),  # Создание вопроса
 ]
